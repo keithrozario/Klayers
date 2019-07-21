13ba0490dbfdc91a74fe00567fa1d15e7b33f5da
keithrozario <keithjosephrozario@gmail.com>
2019-07-21 22:27:25 +0800

    included step function onto serverless.yml

--- serverless/invoke_pipeline.py
+++ serverless/invoke_pipeline.py
@@ -1,6 +1,28 @@
+import os
+import json
+import uuid
+
 import boto3
 
 
 def main(event, context):
 
-    return True
+    """
+    Args:
+      package: Python Package to build and deploy
+    return:
+      execution_arn: ARN of the state machine execution that is building the package
+    """
+
+    package = event['package']
+
+    client = boto3.client('stepfunctions')
+    response = client.start_execution(
+        stateMachineArn=os.environ['PIPELINE_ARN'],
+        name=str(uuid.uuid4()).replace('-', 'X'),  # name cannot contain '-'
+        input=json.dumps({"input": {"package": package}})
+    )
+
+    execution_arn = response['executionArn']
+
+    return execution_arn
