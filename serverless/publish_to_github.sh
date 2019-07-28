2bf481b41c89ab37585b302bab1b41baa0fa0360
keithrozario <keithjosephrozario@gmail.com>
2019-07-28 15:02:19 +0800

    added boto3

--- serverless/publish_to_github.sh
+++ serverless/publish_to_github.sh
@@ -1,6 +1,7 @@
 handler () {
     set -e
     cd /tmp
-    git clone https://github.com/keithrozario/S3-71
-    ls -lah /tmp
+    git clone https://github.com/keithrozario/Klayers
+    aws s3 cp 
+
 }
