258c91c9f50d46f451d6a8a6270863166c384000
keithrozario <keithjosephrozario@gmail.com>
2019-07-28 15:15:24 +0800

    publish to github

--- serverless/publish_to_github.sh
+++ serverless/publish_to_github.sh
@@ -2,6 +2,20 @@ handler () {
     set -e
     cd /tmp
     git clone https://github.com/keithrozario/Klayers
-    aws s3 cp 
+
+    cd Klayers
+    aws s3 cp s3://klayers-bucket--prod/packages packages --recursive
+    aws s3 cp s3://klayers-bucket--prod/arns arns --recursive
+
+    COMMITDATE=$(date +%x_%H:%M)
+    git add -A
+    git commit -m "$COMMITDATE"
+    echo $COMMITDATE
+
+    SSH_KEY=$(aws secretsmanager get-secret-value --secret-id /Klayers-python37/Klayers-prod/github_deploy_key | jq -r .SecretString | jq -r .sshkey)
+    echo $SSH_KEY >> /tmp/.ssh_key
+    chmod 400 /tmp/.ssh_key
+
+    ls -lah /tmp
 
 }
