28100f6285cdf4c85f6f3fd7cc6250b09d55ab30
keithrozario <keithjosephrozario@gmail.com>
2019-08-03 16:57:55 +0800

    Added SSH keys to GitHub

--- update_arns_packages.sh
+++ update_arns_packages.sh
@@ -2,3 +2,5 @@
 
 aws s3 cp s3://klayers-bucket--prod/packages packages --profile LayerUploader --recursive
 aws s3 cp s3://klayers-bucket--prod/arns arns --profile LayerUploader --recursive
+
+# https://security.stackexchange.com/questions/29106/openssl-recover-key-and-iv-by-passphrase/29139#29139
