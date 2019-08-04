eed7bd0c36f7565c37c64439c3d6e66a286e7a80
keithrozario <keithjosephrozario@gmail.com>
2019-08-04 12:42:53 +0800

    last entry

--- infra/key_gen/gen_key.sh
+++
@@ -1,5 +0,0 @@
-mkdir $1
-cd $1
-ssh-keygen -t rsa -b 4096 -f ./github_id_rsa
-openssl rand -base64 -out secret.key 48
-openssl aes-256-cbc -e -in github_id_rsa -out github_id_rsa.enc -k file:./secret.key
