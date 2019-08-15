3dd0777a15d09f12f4f32292741af9e4ddc71484
keithrozario <keithjosephrozario@gmail.com>
2019-08-15 23:55:11 +0800

    added stream processor

--- serverless/stream_processor.py
+++ serverless/stream_processor.py
@@ -1,11 +1,51 @@
 import logging
+import json
+
+
+import boto3
+from botocore.exceptions import ClientError
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
 def main(event, context):
 
-    print(event)
+    records = event.get('Records', [])
+    logger.info(f"{len(records)} to process")
 
-    return "Done"
+    for record in records:
+        logger.info(f"Removal of f{json.dumps(record.get('dynamodb').get('Keys'))}")
+        if record['eventName'] == 'REMOVE':
+            layer_version_arn = remove(record)
+
+        else:
+            logger.info(f"No processing required for event type: {record['eventName']}")
+
+    return {"status": "Done"}
+
+
+def remove(record):
+
+    layer_version_arn = record['dynamodb']['OldImage']['layer_version_arn']['S']
+    logger.info(f"Deleting: {layer_version_arn}")
+
+    arn_elements = layer_version_arn.split(":")
+    region = arn_elements[3]
+    layer_name = arn_elements[6]
+    layer_version = int(arn_elements[7])
+
+    client = boto3.client('lambda', region_name=region)
+
+    try:
+        client.delete_layer_version(
+            LayerName=layer_name,
+            VersionNumber=layer_version
+        )
+        logger.info(f"Successfully deleted {layer_version_arn}")
+    except AttributeError:
+        logger.error(f"Unable to delete layer {layer_version_arn}")
+    except ClientError as e:
+        logger.error(f"{e.response['Error']['Code'] : {e.response['Error']['Message']}}")
+
+    return layer_version_arn
