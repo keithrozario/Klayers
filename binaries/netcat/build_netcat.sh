040cc793b5bc9c3a74349d9a88563a46b861cb5d
keithrozario <keithjosephrozario@gmail.com>
2019-12-08 18:28:48 +0800

    new layout

--- binaries/netcat/build_netcat.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-rm -rf layer
-docker build -t keithrozario/netcat .
-CONTAINER=$(docker run -d keithrozario/netcat:latest false)
-docker cp $CONTAINER:/tmp/layer/netcat.zip netcat.zip
-docker rm $CONTAINER
--- binaries/openssl/build_openssl.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-rm -rf layer
-docker build -t keithrozario/openssl .
-CONTAINER=$(docker run -d keithrozario/openssl:latest false)
-docker cp $CONTAINER:/tmp/layer/openssl.zip openssl.zip
-docker rm $CONTAINER
