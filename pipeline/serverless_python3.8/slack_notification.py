9db633aa30b44ab957cd0c190326064bacc38f70
keithrozario <keithjosephrozario@gmail.com>
2020-02-03 23:08:40 +0800

    attached notifications to failed pipelines

--- pipeline/serverless_python3.8/slack_notification.py
+++ pipeline/serverless_python3.8/slack_notification.py
@@ -9,7 +9,9 @@ import boto3
 Cold start code
 """
 logger = logger_setup()
+channel = f"#buildstatus-{os.environ['STAGE'][8:][:-3]}"
 
+# Get Slack Token
 ssm_client = boto3.client('ssm')
 response = ssm_client.get_parameter(
     Name=os.environ.get('SLACK_TOKEN_PARAMETER'),
@@ -20,10 +22,9 @@ client = slack.WebClient(token=slack_token)
 
 
 @logger_inject_lambda_context
-def slack_notification(event, context):
+def slack_notification_test(event, context):
 
     message = event['message']
-    channel = f"buildstatus-{os.environ['STAGE'][8:][:-3]}"
 
     status = post_to_slack(message=message,
                            channel=channel)
@@ -32,7 +33,40 @@ def slack_notification(event, context):
         logger.info(f"Successfully posted {message} to {channel}")
         status = "Success"
     else:
-        logger.error(f"Failed to post {message} to {channel}")
+        logger.error(f"Failed to post Message:{message} to Channel:#{channel}")
+        status = "Failed"
+
+    return json.dumps({"status": status})
+
+
+@logger_inject_lambda_context
+def slack_notification_pipeline(event, context):
+
+    """
+    Post error messages for various failed statuses of Pipeline step function to slack
+    event: see https://docs.aws.amazon.com/step-functions/latest/dg/cw-events.html
+    """
+
+    status = event.get('detail', {}).get('status')
+    package = json.loads(event.get('detail', {}).get('input')).get('package')
+
+    if status in ['TIMED_OUT', 'ABORTED', 'FAILED']:
+        message = f"ERROR: Building {package} status: {status}"
+    elif status == 'SUCCEEDED':
+        message = f"GOOD: Building {package} status: {status}"
+    elif status == 'RUNNING':
+        message = f"INFO: Building {package} status: {status}"
+    else:
+        message = f"Unknown Status {status}"
+
+    status = post_to_slack(message=message,
+                           channel=channel)
+
+    if status:
+        logger.info(f"Successfully posted {message} to {channel}")
+        status = "Success"
+    else:
+        logger.error(f"Failed to post Message:{message} to Channel:#{channel}")
         status = "Failed"
 
     return json.dumps({"status": status})
