b4901a0f77fd9f9c69411860a3e47a4de00933b0
keithrozario <keithjosephrozario@gmail.com>
2020-04-26 01:38:51 +0800

    Externalized Config

--- pipeline/serverless_python3.8/invoke_pipeline.py
+++ pipeline/serverless_python3.8/invoke_pipeline.py
@@ -1,35 +1,70 @@
 import os
 import json
-from datetime import datetime
 
 import boto3
 
 import get_config
+from aws_lambda_powertools.logging import logger_setup, logger_inject_lambda_context
+logger = logger_setup(service_name="Invoke Pipelines")
 
 
+def log_eventbridge_errors(response, function_logger):
+    """
+    Args:
+          response: Response from putting events onto eventBridge
+          function_logger: logger to write out errors to (if any exists)
+    return:
+        null
+    """
+
+    if response['FailedEntryCount'] > 0:
+        for entry in response['Entries']:
+            if entry.get('ErrorCode', False):
+                logger.error(entry)
+    return
+
+
+@logger_inject_lambda_context
 def main(event, context):
 
     """
     Args:
       package: Python Package to build and deploy
     return:
-      execution_arn: ARN of the state machine execution that is building the package
+      response: Entries in EventBridge for processing
     """
 
     packages = get_config.get_packages()
+    client = boto3.client('events')
+    stage = os.environ['STAGE']
 
-    execution_arns =[]
+    entries = []
+    logger.info(f"Preparing {len(packages)} packages")
 
+    # post message to EventBridge to trigger step functions
     for package in packages:
 
-        client = boto3.client('stepfunctions')
-        execution_time = datetime.now().isoformat().replace('-', '').replace(':', '')[:14]
-        response = client.start_execution(
-            stateMachineArn=os.environ['PIPELINE_ARN'],
-            name=f"{package}_{execution_time}",
-            input=json.dumps({"package": package})
-        )
+        entry = {
+            'Source': f"Klayers.invoke.{stage}",
+            'Resources': [],
+            'DetailType': "invoke_pipeline",
+            'Detail': json.dumps({"package": package}),
+            'EventBusName': 'default'
+        }
+        entries.append(entry)
+    response = client.put_events(Entries=entries)
+    log_eventbridge_errors(response, logger)
 
-        execution_arns.append(response['executionArn'])
+    # Post Status to Slack
+    message = f"Started build on {len(packages)} packages, with {response['FailedEntryCount']} errors"
+    entry = {
+        'Source': f"Klayers.invoke.{stage}",
+        'Resources': [],
+        'DetailType': "post_to_slack",
+        'Detail': json.dumps({"message": message}),
+        'EventBusName': 'default'
+    }
+    slack_response = client.put_events(Entries=[entry])
+    log_eventbridge_errors(slack_response, logger)
 
-    return {"arns": execution_arns}
+    return response
