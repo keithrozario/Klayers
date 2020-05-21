b78897a9fdbc7b2f3d391bebf4d071d355102fdd
keithrozario <keithjosephrozario@gmail.com>
2020-05-21 23:34:05 +0800

    tidied up Terraform

--- pipeline/infra/dynamodb.tf
+++
@@ -1,6 +0,0 @@
-module "dynamo_table" {
-  source              = "./dynamodb"
-  table_logical_name = "db"
-  app_name = lookup(var.app_name, terraform.workspace)
-  workspace_name = terraform.workspace
-}
