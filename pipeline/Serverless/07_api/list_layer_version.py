63383aa56ece1f630125d899e611cbdd18514a55
keithrozario <keithjosephrozario@gmail.com>
2020-07-29 09:18:38 +0800

    added list layers

---
+++ pipeline/Serverless/07_api/list_layer_version.py
@@ -0,0 +1,64 @@
+import os
+import json
+
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+from aws_lambda_powertools.logging import Logger
+logger = Logger()
+
+from common.dynamodb import DecimalEncoder, map_keys
+
+
+def query_table(region, table, pk):
+    """
+    Args:
+      table: DynamoDB table object to query
+      region: region to query on
+    returns:
+      items: items returned from the query
+    """
+
+    kwargs = {
+        "KeyConditionExpression": Key("pk").eq(pk),
+        "ProjectionExpression": "arn, pckgVrsn, dplySts, rqrmntsTxt, exDt",
+        "FilterExpression": "attribute_exists(dplySts)"  # don't get latest version
+    }
+    items = []
+
+    while True:
+        response = table.query(**kwargs)
+        items.extend(response["Items"])
+
+        try:
+            kwargs["ExclusiveStartKey"] = response["ExclusiveStartKey"]
+        except KeyError:
+            logger.info({
+                "region": region,
+                "num_items": len(items),
+                "message": "Completed Query for objects in region"
+            }
+            )
+            break
+
+    return map_keys(items)
+
+@logger.inject_lambda_context
+def main(event, context):
+    """
+    Gets layer arns for each region and publish to S3
+    """
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ["DB_NAME"])
+    # region = event.get('pathParameters').get('region')
+    # package = event.get('pathParameters').get('package')
+    region = "ap-southeast-1"
+    package = "requests"
+    pk = f"lyr#{region}.{package}"
+    api_response = query_table(table=table, region=region, pk=pk)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(api_response, cls=DecimalEncoder),
+    }
