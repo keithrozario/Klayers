0cc5781171c7f66fd0a2c426a47fff3fdf36f242
Keith Rozario <keithjosephrozario@gmail.com>
2023-04-22 13:07:02 +0800

    build images - plural'

--- pipeline/container_images/build_images/deploy_all.sh
+++ pipeline/container_images/build_images/deploy_all.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 ## First Argument to the script is the $STAGE, all other arguments are then taken from the configuration file
-## e.g. $ ./deploy-container.sh Klayers-defaultp38
+## e.g. $ ./deploy-all.sh Klayers-defaultp38
 
 STAGE="${1:-Klayers-defaultp38}"
 for d in ./*/ ; do (cd "$d" && ./deploy-container.sh $STAGE); done
