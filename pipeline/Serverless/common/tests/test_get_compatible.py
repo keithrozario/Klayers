953590f99c51469304769b8f4453ae1e84cc09ea
Keith Rozario <keithjosephrozario@gmail.com>
2023-04-24 17:32:51 +0800

    fixed capital P

--- pipeline/Serverless/common/tests/test_get_compatible.py
+++ pipeline/Serverless/common/tests/test_get_compatible.py
@@ -1,10 +1,10 @@
 from .. import get_compatible
 
 def test_runtimes():
-    assert get_compatible.get_compatible_runtimes('p3.8') == ['Python3.8']
-    assert get_compatible.get_compatible_runtimes('p3.10') == ['Python3.10']
-    assert get_compatible.get_compatible_runtimes('p3.10-arm64') == ['Python3.10']
-    assert get_compatible.get_compatible_runtimes('p3.10-x86') == ['Python3.10']
+    assert get_compatible.get_compatible_runtimes('p3.8') == ['python3.8']
+    assert get_compatible.get_compatible_runtimes('p3.10') == ['python3.10']
+    assert get_compatible.get_compatible_runtimes('p3.10-arm64') == ['python3.10']
+    assert get_compatible.get_compatible_runtimes('p3.10-x86') == ['python3.10']
 
 def test_architecture():
     assert get_compatible.get_compatible_architectures('p3.8') == ['x86_64']
