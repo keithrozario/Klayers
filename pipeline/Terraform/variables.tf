956aebdc8b3b9238cbef4845677c180eba856049
Keith Rozario <keithjosephrozario@gmail.com>
2023-04-28 13:35:22 +0800

    removed Klayers prefix

--- pipeline/Terraform/variables.tf
+++ pipeline/Terraform/variables.tf
@@ -1,7 +1,6 @@
 variable "app_name" { type = string }
 variable "aws_region" { type = map(any) }
 variable "aws_profile" { type = map(any) }
-variable "lambda_prefix" { type = map(any) }
 variable "github_repo" { type = string }
 variable "api_domain_name" { type = map(any) }
 variable "s3bucket_config_parameter_name_suffix" { type = string }
