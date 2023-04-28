e2cc6ca010cc8e33bee7c06b2774900a55b5f657
Keith Rozario <keithjosephrozario@gmail.com>
2023-04-28 14:09:38 +0800

    refactored to call common services

--- pipeline/Terraform/variables.tf
+++ pipeline/Terraform/variables.tf
@@ -3,5 +3,6 @@ variable "aws_region" { type = map(any) }
 variable "aws_profile" { type = map(any) }
 variable "github_repo" { type = string }
 variable "api_domain_name" { type = map(any) }
+variable "cron_expression_invoke_pipelines" { type = map(any) }
 variable "s3bucket_config_parameter_name_suffix" { type = string }
 variable "github_role_name" { type = map(any) }
