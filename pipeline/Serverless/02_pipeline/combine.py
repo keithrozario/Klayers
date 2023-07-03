47dc8042699b7baf08cb74e421081a1402066fca
Keith Rozario <keithjosephrozario@gmail.com>
2023-07-03 17:07:46 +0800

    fixed permissions

--- pipeline/Serverless/02_pipeline/combine.py
+++ pipeline/Serverless/02_pipeline/combine.py
@@ -4,6 +4,9 @@ from zipfile import ZipFile
 
 import boto3
 
+from common.get_config import get_from_common_service
+from common.get_compatible import get_compatible_runtimes, get_compatible_architectures
+
 from aws_lambda_powertools.logging import Logger
 
 logger = Logger()
@@ -17,34 +20,32 @@ def main(event, context):
     Combines multiple packages into a single zip file
     Function downloads layer for packages from S3, unzips them, and combines them all into a single zip
     Uploads zip into S3 with a new name
-    Does **not** deploy, Does **not** write to Dynamo
+    Deploys to all regions
     """
 
     packages = event["packages"]
-    version = "1.0-man"
-    build_flag = False
-    name = combined_name(packages)
+    combined_package_name = combined_name(packages)
     python_version = event["python_version"]
-    zip_file_S3key = f"{python_version}/{name}.zip"
-    requirements_hash = "n.a custom build"
+    zip_file_S3key = f"{python_version}/{combined_package_name}.zip"
     license_info = "Refer for individual package"
-    force_deploy = True
 
     archive_path = combine_packages(
-        packages=packages, python_version=python_version, combined_name=name
+        packages=packages, python_version=python_version, combined_name=combined_package_name
     )
     upload_to_s3(archive_path=archive_path, s3_location=zip_file_S3key)
 
-    return {
-        "zip_file_S3key": zip_file_S3key,
-        "package": name,
-        "version": version,
-        "requirements_hash": requirements_hash,
-        "license_info": license_info,
-        "build_flag": build_flag,
-        "force_deploy": force_deploy,
-        "python_version": python_version,
-    }
+    regions = get_from_common_service(resource=f"/api/v1/config/{python_version}/rgns")
+    logger.info({"regions": regions})
+
+    layers = publish_layer(
+        regions=regions,
+        archive_path=archive_path,
+        python_version=python_version,
+        license_info=license_info,
+        combined_name=combined_package_name
+    )
+
+    return layers
 
 
 def combined_name(packages: list[str]) -> str:
@@ -120,3 +121,63 @@ def upload_to_s3(archive_path: str, s3_location: str):
     s3.upload_file(archive_path, bucket_name, s3_location)
 
     return None
+
+
+def publish_layer(regions: list[str], archive_path: str, python_version: str, combined_name: str, license_info: str):
+    """
+    Args:
+        regions: List of regions to deploy to
+        archive_path: Location of zip file to be uploaded to S3 bucket
+        combined_name: Name of combined package
+    returns:
+        layer_arns: List of layer ARNs deployed
+    """
+
+
+    layer_name = (
+        f"{os.environ['LAMBDA_LAYER_PREFIX']}{python_version.replace('.','')}-{combined_name}"
+    )
+
+    with open(archive_path, "rb") as zip_file:
+        zip_binary = zip_file.read()
+
+    layer_arns = []
+
+    for region in regions:
+        logger.info({"message": "Deploying", "region": region, "package": combined_name})
+        lambda_client = boto3.client("lambda", region_name=region)
+        response = lambda_client.publish_layer_version(
+            LayerName=layer_name,
+            Description=f"{combined_name}",
+            Content={"ZipFile": zip_binary},
+            CompatibleRuntimes=get_compatible_runtimes(python_version=python_version),
+            CompatibleArchitectures=get_compatible_architectures(
+                python_version=python_version
+            ),
+            LicenseInfo=license_info,
+        )
+        layer_version_arn = response["LayerVersionArn"]
+        layer_arns.append(layer_version_arn)
+        layer_version = int(layer_version_arn.split(":")[-1])
+
+        # Make Layer Publicly accessible
+        logger.info(
+            {
+                "message": "Making Public",
+                "region": region,
+                "package": combined_name,
+                "python_version": python_version,
+                "arn": layer_version_arn
+            }
+        )
+        lambda_client.add_layer_version_permission(
+            LayerName=layer_name,
+            VersionNumber=layer_version,
+            StatementId="make_public",
+            Action="lambda:GetLayerVersion",
+            Principal="*",
+        )
+
+    logger.info({"message": "Layer ARNs", "layer_arns": layer_arns})
+
+    return layer_arns
