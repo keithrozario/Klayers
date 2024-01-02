e4962f784369c77629fd974bdf0994c56a806c8f
Keith Rozario <keithjosephrozario@gmail.com>
2024-01-02 16:07:14 +0800

    removed 3.9 repo

--- pipeline/Terraform/container_repositories.tf
+++ pipeline/Terraform/container_repositories.tf
@@ -1,24 +1,5 @@
 # Container Build Images
 
-## Python 3.9 - x86 (only)
-resource "aws_ecr_repository" "p39build_x86" {
-  name                 = "p39build"
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-resource "aws_ssm_parameter" "p39_build_repo" {
-  type        = "String"
-  description = "URL for p39 x86 repo"
-  name        = "/${var.app_name}/${local.workspace_full_name}/build/p39/x86/repo"
-  value       = aws_ecr_repository.p39build_x86.repository_url
-  overwrite   = true
-}
-
 ## Python 3.10 - x86
 resource "aws_ecr_repository" "p310build_x86" {
   name                 = "p310build"
